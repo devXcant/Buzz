6341bdbed1f169e7df3b95616e703bd4417ac2ce
devXcant <devxcant@gmail.com>
2024-05-25 15:27:20 +0100

    backend setup

---
+++ server/index.js
@@ -0,0 +1,55 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from 'cors'
+import dotenv  from "dotenv";
+import multer from "multer";
+import helmet, { crossOriginResourcePolicy } from 'helmet';
+import morgan from 'morgan'
+import path from 'path'
+import {fileURLToPath} from 'url'
+import authRoutes from './routes/auth.js'
+
+import userRoutes from './routes/users.js'
+import { register } from "./controllers/auth.js";
+
+
+// CONFIGURATIONS FOR THE PROJECT
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet(crossOriginResourcePolicy({policy: "cross-origin"})));
+app.use(morgan("common"));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
+
+
+
+// FILE STORAGES
+const storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, "public/assets");
+    },
+    filename: function(req, file, cb){
+        cb(null, file.originalname)
+    }
+});
+
+const upload = multer({storage})
+
+// ROUTES WITH FILES
+app.post("/auth/register", upload.single("picture"), register)
+
+
+// ROUTES
+app.use('/auth', authRoutes)
+app.use("/users", userRoutes)
+
+
+// SETTING UP MONGOOSE
+const PORT = process.env.PORT || 6001;
+mongoose.connect(process.env.MONGO_URL).then(() => {
+        app.listen(PORT, ()=> console.log(`Server PORT: ${PORT}`));
+}).catch((error) => console.log(`${error} did not connect`))
