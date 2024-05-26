7e54ba1bbd362f9b3aedf1232bdb80e2bc27df4a
devXcant <devxcant@gmail.com>
2024-05-26 20:00:36 +0100

    public folder

--- server/index.js
+++ server/index.js
@@ -8,9 +8,10 @@ import morgan from 'morgan'
 import path from 'path'
 import {fileURLToPath} from 'url'
 import authRoutes from './routes/auth.js'
-
 import userRoutes from './routes/users.js'
+import postRoutes from './routes/posts.js'
 import { register } from "./controllers/auth.js";
+import { verifyToken } from "./middleware/auth.js";
 
 
 // CONFIGURATIONS FOR THE PROJECT
@@ -40,12 +41,14 @@ const storage = multer.diskStorage({
 const upload = multer({storage})
 
 // ROUTES WITH FILES
-app.post("/auth/register", upload.single("picture"), register)
+app.post("/auth/register", upload.single("picture"), register);
+app.post("?posts", verifyToken, upload.single("picture"),);
 
 
 // ROUTES
 app.use('/auth', authRoutes)
 app.use("/users", userRoutes)
+app.use("/posts", postRoutes)
 
 
 // SETTING UP MONGOOSE
@@ -53,3 +56,4 @@ const PORT = process.env.PORT || 6001;
 mongoose.connect(process.env.MONGO_URL).then(() => {
         app.listen(PORT, ()=> console.log(`Server PORT: ${PORT}`));
 }).catch((error) => console.log(`${error} did not connect`))
+ 
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -6,3 +6,5 @@ const router = express.Router();
 
 
 router.post("/login", login)
+
+export default router;
